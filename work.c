e31179c56716c2e4acd40cd97b6cc9cd2deb3d24
dl5 <user.dl5>
2023-03-27 11:24:08 +0400

    copy of main with some function

--- work.c
+++ work.c
@@ -2,7 +2,7 @@
 #include "work.h"
 
 void some_function() {
-  printf("hello from some_function()\n");
+  printf("hello from main some_function()\n");
   for (int i = 0; i < 100; i++) {
     printf("%d\n", i);
   }
